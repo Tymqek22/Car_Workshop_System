54e6c56e10062f8f2a6fda9f4a4881a9df6477c7
Tymqek22 <133526709+Tymqek22@users.noreply.github.com>
2026-02-15 18:02:34 +0100

    Add Jwt service

---
+++ src/Car_Workshop_System.Infrastructure/Auth/Services/JwtService.cs
@@ -0,0 +1,43 @@
+﻿using Car_Workshop_System.Infrastructure.Auth.Identity.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Car_Workshop_System.Infrastructure.Auth.Services
+{
+	public class JwtService
+	{
+		private readonly IConfiguration _config;
+
+		public JwtService(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public string GenerateToken(ApplicationUser user, IList<string> roles)
+		{
+			var claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.NameIdentifier, user.Id),
+				new Claim(ClaimTypes.Name, user.UserName)
+			};
+
+			claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role,r)));
+
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+			var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				issuer: _config["Jwt:Issuer"],
+				audience: _config["Jwt:Audience"],
+				claims: claims,
+				expires: DateTime.Now.AddHours(8),
+				signingCredentials: creds);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+	}
+}
