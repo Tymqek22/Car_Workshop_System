18bddd069b5485c473f14eb62bf2b2e753e8c274
Tymqek22 <133526709+Tymqek22@users.noreply.github.com>
2026-02-17 16:37:51 +0100

    Add IdentityService in infrastructure layer

---
+++ src/Car_Workshop_System.Infrastructure/Auth/Services/IdentityService.cs
@@ -0,0 +1,26 @@
+﻿using Car_Workshop_System.Application.Interfaces;
+using Car_Workshop_System.Infrastructure.Auth.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Car_Workshop_System.Infrastructure.Auth.Services
+{
+	public class IdentityService : IIdentityService
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public IdentityService(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public async Task<string> GetUserById(string id)
+		{
+			var user = await _userManager.FindByIdAsync(id);
+
+			if (user is null)
+				return null;
+
+			return user.Id;
+		}
+	}
+}
