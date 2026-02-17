999bea6744a4c0296552f0b2f7578e127a0e62bf
Tymqek22 <133526709+Tymqek22@users.noreply.github.com>
2026-02-17 14:34:57 +0100

    Add Generic repository

---
+++ src/Car_Workshop_System.Application/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Car_Workshop_System.Application.Interfaces
+{
+	public interface IRepository<T>
+	{
+		public Task<T> GetByIdAsync(int id);
+		public Task AddAsync(T entity);
+		public Task UpdateAsync(T entity);
+		public Task DeleteAsync(T entity);
+		public Task SaveChangesAsync();
+	}
+}
---
+++ src/Car_Workshop_System.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Car_Workshop_System.Application.Interfaces;
+using Car_Workshop_System.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Workshop_System.Infrastructure.Repository
+{
+	public class Repository<T> : IRepository<T>
+		where T : class
+	{
+		private readonly ApplicationDbContext _dbContext;
+		private readonly DbSet<T> _dbSet;
+
+		public Repository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+			_dbSet = dbContext.Set<T>();
+		}
+
+		public async Task AddAsync(T entity)
+		{
+			await _dbSet.AddAsync(entity);
+		}
+
+		public async Task DeleteAsync(T entity)
+		{
+			_dbSet.Remove(entity);
+		}
+
+		public async Task<T> GetByIdAsync(int id)
+		{
+			return await _dbSet.FindAsync(id);
+		}
+
+		public async Task SaveChangesAsync()
+		{
+			await _dbContext.SaveChangesAsync();
+		}
+
+		public async Task UpdateAsync(T entity)
+		{
+			_dbSet.Update(entity);
+		}
+	}
+}
