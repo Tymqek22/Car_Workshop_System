260562e87e6cdbb84c474522046b6819a2dd1420
Tymqek22 <133526709+Tymqek22@users.noreply.github.com>
2026-02-26 15:21:22 +0100

    Add error and result classes

---
+++ src/Car_Workshop_System.Application/Common/Result.cs
@@ -0,0 +1,19 @@
+﻿using Car_Workshop_System.Domain.Errors;
+
+namespace Car_Workshop_System.Application.Common
+{
+	public class Result
+	{
+		public bool IsSuccess { get; }
+		public Error? Error { get; }
+
+		public Result(bool success, Error? error)
+		{
+			IsSuccess = success;
+			Error = error;
+		}
+
+		public static Result Success() => new(true,null);
+		public static Result Failure(Error error) => new(false,error);
+	}
+}
---
+++ src/Car_Workshop_System.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,22 @@
+﻿namespace Car_Workshop_System.Domain.Errors
+{
+	public record Error(string Code, string Description);
+
+	public static class WorkOrderErrors
+	{
+		public static readonly Error OrderNotFound =
+			new("WorkOrderErrors.OrderNotFound","Work order doesn't exist.");
+
+		public static readonly Error TechnicianNotFound =
+			new("WorkOrderErrors.TechnicianNotFound","Technician doesn't exist.");
+
+		public static readonly Error TechnicianAlreadyAssigned =
+			new("WorkOrderErrors.TechnicnianAlreadyAssigned","This technician has been already assigned to this order.");
+
+		public static readonly Error AlreadyCancelled =
+			new("WorkOrderErrors.AlreadyCancelled","Work order is already cancelled.");
+
+		public static readonly Error WrongStatusPicked =
+			new("WorkOrderErrors.WrongStatusPicked","Status cannot be changed. Try to pick different status.");
+	}
+}
